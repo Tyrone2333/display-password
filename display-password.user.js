3e24867fc6045ec8e7d2f9fcc97d13194b7d1c8f
en20 <424021424@qq.com>
2019-03-23 15:35:32 +0800

    更改主页为 github

--- display-password.user.js
+++ display-password.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         双击显示密码
-// @namespace    http://undefined.ga
-// @version      1.0.0
+// @namespace    https://github.com/Tyrone2333/display-password
+// @version      1.0.1
 // @description  双击显示密码,失去焦点隐藏
 // @author       en20
 // @include      http*://*
